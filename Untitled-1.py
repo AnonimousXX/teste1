9a5279420b78f6cea55a3adadc822f4be3089a1c
AnonimousXX <140027388+AnonimousXX@users.noreply.github.com>
2023-07-19 20:56:29 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,35 @@
+# nome = input("Digite seu nome: ")
+# print (f"Olá {nome}! Seja bem-vindo(a).")
+
+# nome = input("Digite seu nome: ")
+# idade = input("Digite sua idade: ")
+# cidade = input("Digite sua cidade: ")
+# frase = f"Olá {nome}! Você tem {idade} anos e mora em {cidade}."
+# print (frase)
+
+# a = input("Digite o valor de A: ")
+# b = input("Digite o valor de B: ")
+# aux = a
+# a = b
+# b = aux
+# print ("Valores trocados: ")
+# print (f"A = {a}")
+# print (f"B = {b}")
+
+# a = int(input("Digite o valor de A: "))
+# b = int(input("Digite o valor de B: "))
+# c = int(input("Digite o valor de C: "))
+# d = int(input("Digite o valor de D: "))
+# p = a * c
+# s = b + d
+# print (f"Produto de A e C é: {p}")
+# print (f"A soma de B e D é: {s}")
+
+# tempo = float(input("Digite o tempo (horas): "))
+# velocidade = float(input("Digite a velocidade média: "))
+# distancia = tempo * velocidade
+# litros_usados = distancia / 12
+# print (f"Velocidade média: {velocidade} kmh")
+# print (f"tempo gasto na viagem: {tempo} horas")
+# print (f"Distancia percorrida: {distancia} Km")
+# print (f"quantidade litros: {litros_usados:.2f}")
